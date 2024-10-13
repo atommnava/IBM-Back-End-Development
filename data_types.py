9321205f8b19a18f034ec01ce868d6323f39deec
Atom M. Nava <atommunoznava@gmail.com>
2024-10-13 13:13:47 -0600

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,42 @@
+# Integer
+
+11
+# Float
+
+2.14
+# String
+
+"Hello, Python 101!"
+# Type of 12
+
+type(12)
+# Type of 2.14
+
+type(2.14)
+# Type of "Hello, Python 101!"
+
+type("Hello, Python 101!")
+# Write your code below. Don't forget to press Shift+Enter to execute the cell
+
+# Integers 
+# Print the type of 4
+
+type(4)
+
+# Floats
+# Print the type of 1.0
+
+type(1.0) # Notice that 1 is an int, and 1.0 is a float
+
+# Converting from one object type to a different object type
+# Verify that this is an integer
+
+type(2)
+
+# Convert 2 to a float
+
+float(2)
+
+# Convert integer 2 to a float and check its type
+
+type(float(2))
